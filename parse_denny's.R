2bca6d07bf46769d1f121fe86fb8ae66d2ef5138
Dewen Xu <tracyx008@gmail.com>
2016-10-17 09:57:19 -0400

    ...

--- parse_denny's.R
+++ parse_denny's.R
@@ -2,14 +2,38 @@ library(rvest)
 dennys_info_LA = read_html("data/dennys/LA.xml") 
 dennys_info_Durham = read_html("data/dennys/Durham.xml")
 
-location_name = dennys_info_LA %>% html_nodes("name") %>% html_text() %>% str_trim()
-address = dennys_info %>% html_nodes("address1") %>% html_text() %>% str_trim()
-zipcode = dennys_info_LA %>% html_nodes("postalcode") %>% html_text() %>% str_trim()
-city = dennys_info_LA %>% html_nodes("city") %>% html_text() %>% str_trim()
-state = dennys_info_LA %>% html_nodes("state") %>% html_text() %>% str_trim()
-latitude = dennys_info_LA %>% html_nodes("latitude") %>% html_text() %>% str_trim()
-longitude = dennys_info_LA %>% html_nodes("longitude") %>% html_text() %>% str_trim()
-phone = dennys_info_LA %>% html_nodes("phone") %>% html_text() %>% str_trim()
+location_name = dennys_info_LA %>% 
+  html_nodes("name") %>% 
+  html_text() %>% 
+  str_trim()
+address = dennys_info %>%
+  html_nodes("address1") %>% 
+  html_text() %>% 
+  str_trim()
+zipcode = dennys_info_LA %>% 
+  html_nodes("postalcode") %>%
+  html_text() %>% 
+  str_trim()
+city = dennys_info_LA %>% 
+  html_nodes("city") %>% 
+  html_text() %>% 
+  str_trim()
+state = dennys_info_LA %>% 
+  html_nodes("state") %>% 
+  html_text() %>%
+  str_trim()
+latitude = dennys_info_LA %>% 
+  html_nodes("latitude") %>% 
+  html_text() %>% 
+  str_trim()
+longitude = dennys_info_LA %>% 
+  html_nodes("longitude") %>% 
+  html_text() %>% 
+  str_trim()
+phone = dennys_info_LA %>%
+  html_nodes("phone") %>% 
+  html_text() %>% 
+  str_trim()
 
 res = list()
 for(j in seq_along(address))
@@ -22,6 +46,5 @@ for(j in seq_along(address))
     long = longitude[j]
   )
 }
-
 dennys = bind_rows(res)
 
