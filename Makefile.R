0dcfaca88c71c89a99f2e59121070f065bfce251
Muyao Sun <ms680@duke.edu>
2016-10-18 16:30:58 -0400

    Makefile2

--- Makefile.R
+++ Makefile.R
@@ -1,7 +1,7 @@
 all: hw4.html
 
 hw4.html: hw4.Rmd data/lq.Rdata data/dennys.Rdata
-  Rscript -e "library(rmarkdown);Team6_hw4('hw4.Rmd')"
+  Rscript -e "library(rmarkdown);render('hw4.Rmd')"
 
 data/lq.Rdata: parse_lq.R data/lq/*.html
   Rscript parse_lq.R
