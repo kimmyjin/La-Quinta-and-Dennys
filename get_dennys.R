3ed020a7b6c47488ec763c23c1fff304cafa7b07
Kim Jin <xj24@duke.edu>
2016-10-19 14:41:36 -0400

    dennys comments

--- get_dennys.R
+++ get_dennys.R
@@ -2,7 +2,7 @@ limit = 1000
 zip_code = 27705
 radius = 3000
 
-get_url = function(limit, zip_code, radius)
+get_url = function(limit, zip_code, radius)         #create the get_url function using limit, zip code and radius
 {
   paste0(
     "https://hosted.where2getit.com/dennys/responsive/ajax?&xml_request=%3Crequest%3E%3Cappkey%3E6B962D40-03BA-11E5-BC31-9A51842CA48B%3C%2Fappkey%3E%3Cformdata+id%3D%22locatorsearch%22%3E%3Cdataview%3Estore_default%3C%2Fdataview%3E%3Climit%3E",
@@ -13,15 +13,15 @@ get_url = function(limit, zip_code, radius)
     "%3C%2Fsearchradius%3E%3C%2Fformdata%3E%3C%2Frequest%3E"
   )
 }
-#Anchorage = get_url
-LA = get_url(limit=1000, zip_code=90210, radius=5000)
-Hawaii = get_url(limit=1000, zip_code=96701, radius=5000)
-Utah = get_url(limit=1000, zip_code=84101, radius=5000)
-Washington = get_url(limit=1000, zip_code=20001, radius=5000)
 
-dir.create("data/dennys",recursive = TRUE, showWarnings = FALSE)
+LA = get_url(limit=1000, zip_code=90210, radius=5000)     # get 1000 restaurants' information within radius of 5000 of LA
+Hawaii = get_url(limit=1000, zip_code=96701, radius=5000)     # get 1000 restaurants' information within radius of 5000 of Hawaii
+Utah = get_url(limit=1000, zip_code=84101, radius=5000)      # get 1000 restaurants' information within radius of 5000 of Utah
+Washington = get_url(limit=1000, zip_code=20001, radius=5000)     # get 1000 restaurants' information within radius of 5000 of Washington
 
-download.file(LA, dest="data/dennys/LA.xml", quiet = TRUE)
-download.file(Hawaii, dest="data/dennys/Hawaii.xml", quiet = TRUE)
-download.file(Utah, dest="data/dennys/Utah.xml", quiet = TRUE)
-download.file(Washington, dest="data/dennys/Washington.xml", quiet = TRUE)
+dir.create("data/dennys",recursive = TRUE, showWarnings = FALSE)   #create a directory
+
+download.file(LA, dest="data/dennys/LA.xml", quiet = TRUE)   #resource of LA to be downloaded
+download.file(Hawaii, dest="data/dennys/Hawaii.xml", quiet = TRUE)   #resource of Hawaii to be downloaded
+download.file(Utah, dest="data/dennys/Utah.xml", quiet = TRUE)  #resource of Utah to be downloaded
+download.file(Washington, dest="data/dennys/Washington.xml", quiet = TRUE)   #resource of Washington to be downloaded
